eeb2c52bb6814978d79e46cd236ce65801b75db7
tarzx <pl44@st-andrews.ac.uk>
2015-02-24 17:18:09 +0000

    Set up new environment
    change host and database

--- db_config.php
+++ db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "tl29"); // db user
-define('DB_PASSWORD', "*****"); // db password (mention your db password here)
-define('DB_DATABASE', "tl29_db"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_USER', 'pl44'); // db user
+define('DB_PASSWORD', 'zRAf46!G'); // db password (mention your db password here)
+define('DB_DATABASE', 'pl44_db'); // database name
+define('DB_SERVER', 'pl44.mysql.cs.st-andrews.ac.uk'); // db server
 ?>
